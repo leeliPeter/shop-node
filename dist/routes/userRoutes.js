ec0d396ebbaba2596d55b9fb4670d9bea0616f7e
lei23lei <lei23lei91@gmail.com>
2024-09-23 11:17:05 -0400

    11:16 23/09

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -125,9 +125,13 @@ router.post('/logout', (req, res) => {
 // Route to get the current user info if session exists
 router.get('/current-user', (req, res) => {
     if (!req.session.user) {
+        var date = new Date();
+        console.log("Current time: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
         console.log('No session found');
         return res.status(401).json({ message: 'Unauthorized' });
     }
+    var date = new Date();
+    console.log("Current time: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
     console.log('Session found');
     console.log(req.session.user);
     res.json(req.session.user);
